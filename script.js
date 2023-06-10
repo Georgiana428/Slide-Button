7b7cafef84c207bc2fd889d8d5b0b95a292384e4
Jojo <raducostina81@gmail.com>
2023-06-10 23:44:03 +0300

    Asta e butonul

---
+++ script.js
@@ -0,0 +1,6 @@
+/*function toggleMode() {
+  const modeToggle = document.getElementById('mode-toggle');
+  modeToggle.classList.toggle('dark-mode');
+  modeToggle.classList.toggle('slide-right');
+}
+*/
